0cd774b11667d75db460b95fa98765a1eb6aa00c
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2023-09-25 19:07:12 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(1);
+
+  const handleSquare = () => {
+    setCount(count * 2);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>useContext</h1>
+      <p>2の乗数を増やす</p>
+      <p>{count}</p>
+      <button onClick={handleSquare}>＋</button>
     </div>
   );
 }
